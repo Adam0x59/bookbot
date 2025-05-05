651c09eaa3f48789ac162e24d425423bb0f59d20
@Adam0x59 <4dam.5.m@gmail.com>
2025-05-05 14:31:44 +0100

    Added character count fucntion

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from stats import get_num_words
+from stats import get_char_nums
 
 
 def main():
     get_num_words("books/frankenstein.txt")
+    get_char_nums("books/frankenstein.txt")
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,17 @@ def get_num_words(filepath):
     '''
     #Print number of words in list to stdout
     print(f"{word_count} words found in the document") 
+
+def get_char_nums(filepath):
+    char_list = []
+    with open(filepath) as f:
+        char_list = list(f.read())
+    char_numbers = {}
+    for char in char_list:
+        char = char.lower()
+        if char in char_numbers:
+            char_numbers[char] += 1
+        else:
+            char_numbers[char] = 1
+    sorted_char_numbers = dict(sorted(char_numbers.items()))
+    print(sorted_char_numbers)
